2628d384a721b7e9e397831a72eacdf8a37f8faf
arnhom <arnhom@outlook.com>
2025-03-10 11:40:41 -0700

    refactor: reg_x and co was bad idea

--- onscreenController.asm
+++ onscreenController.asm
@@ -9,15 +9,15 @@ b START
 #########################################
 COBJ_CB:
 blrl
-.set  REG_GOBJ,31
+.set REG_GOBJ,31
 
 backup
 
-mr  REG_GOBJ, r3
+mr REG_GOBJ, r3
 
 /*
 # Check if paused
-li  r3,1
+li r3,1
 branchl r12,0x801a45e8
 cmpwi r3,2
 beq COBJ_CB_Exit
@@ -28,7 +28,7 @@ cmpwi r0, 1
 beq COBJ_CB_Exit
 
 # Draw camera
-mr  r3, REG_GOBJ
+mr r3, REG_GOBJ
 branchl r12,0x803910d8
 
 COBJ_CB_Exit:
@@ -62,14 +62,6 @@ backup
 .set REG_COBJ, 26 # camero obj?
 .set REG_GOBJ, 25 # game obj?
 
-.set REG_X, 3
-.set REG_Y, 4
-.set REG_Z, 5
-.set REG_A, 6
-.set REG_B, 7
-.set REG_C, 8
-.set REG_D, 9
-.set REG_E, 10
 .set REG_SCRATCH, 12
 
 # get data address
@@ -79,7 +71,7 @@ mflr REG_DATA_ADDR
 # buil COBJ
 load r3, 0x804d6d5c # idk what any of this does.
 lwz r3, 0x0 (r3)
-load  r4, 0x803f94d0
+load r4, 0x803f94d0
 branchl r12, HSD_ArchiveGetPublicAddress
 lwz r3,0x4(r3)
 lwz r3,0x0(r3)
@@ -87,51 +79,51 @@ branchl r12,0x8036a590 # assumedly creates the cobj
 mr REG_COBJ,r3
 
 # build GOBJ
-li REG_X, 19
-li REG_Y, 20
-li REG_Z, 0
+li r3, 19
+li r4, 20
+li r5, 0
 branchl REG_SCRATCH, GObj_Create
-mr REG_GOBJ, REG_X
+mr REG_GOBJ, r3
 
 # add object
-mr  REG_X, REG_GOBJ
-lbz REG_Y, -0x3E55(r13) # what is this offset?
-mr  REG_Z, REG_COBJ
+mr r3, REG_GOBJ
+lbz r4, -0x3E55(r13) # what is this offset?
+mr r5, REG_COBJ
 branchl REG_SCRATCH, GObj_AddToObj
 
 # Init camera
-mr  REG_X, REG_GOBJ
-bl  COBJ_CB
-mflr  REG_Y
-li  REG_Z, COBJ_GXPRI
+mr r3, REG_GOBJ
+bl COBJ_CB
+mflr r4
+li r5, COBJ_GXPRI
 branchl r12,0x8039075c
 # Store COBJs GXLinks
-load REG_X, 1 << TEXT_GXLINK
-stw REG_X, 0x24 (REG_GOBJ)
+load r3, 1 << TEXT_GXLINK
+stw r3, 0x24 (REG_GOBJ)
 
 #build canvas.
-li  REG_X, 2
-mr  REG_Y,REG_GOBJ
-li  REG_Z, 9
-li  REG_A, 13
-li  REG_B, 0
-li  REG_C, TEXT_GXLINK
-li  REG_D, TEXT_GXPRI
-li  REG_E, COBJ_GXPRI
+li r3, 2
+mr r4,REG_GOBJ
+li r5, 9
+li r6, 13
+li r7, 0
+li r8, TEXT_GXLINK
+li r9, TEXT_GXPRI
+li r10, COBJ_GXPRI
 branchl REG_SCRATCH, 0x803a611c
-mr  REG_CANVAS, REG_X
+mr REG_CANVAS, r3
 
 # build text object
-li REG_X, 2
-mr REG_Y, REG_CANVAS # todo need to create the canvas.
+li r3, 2
+mr r4, REG_CANVAS # todo need to create the canvas.
 branchl REG_SCRATCH, Text_CreateStruct
-mr REG_TEXT_STRUCT, REG_X
+mr REG_TEXT_STRUCT, r3
 
-li REG_X, 0x1
-stb REG_X, 0x48(REG_TEXT_STRUCT) # Fixed Width
-stb REG_X, 0x4A(REG_TEXT_STRUCT) # Set text to align center
-stb REG_X, 0x4C(REG_TEXT_STRUCT) # Unk?
-stb REG_X, 0x49(REG_TEXT_STRUCT) # kerning?
+li r3, 0x1
+stb r3, 0x48(REG_TEXT_STRUCT) # Fixed Width
+stb r3, 0x4A(REG_TEXT_STRUCT) # Set text to align center
+stb r3, 0x4C(REG_TEXT_STRUCT) # Unk?
+stb r3, 0x49(REG_TEXT_STRUCT) # kerning?
 
 # set position and scale
 lfs f1, TEXT_X(REG_DATA_ADDR)
@@ -148,7 +140,7 @@ crset 6 # even code i'm looking at doesn't know what this is.
 lfs f1, FLOAT_ZERO(REG_DATA_ADDR)
 fmr f2, f2
 mr r3, REG_TEXT_STRUCT
-# previously i was literally putting the text in the register.  Now im building the pointer to the text
+# previously i was literally putting the text in the register. Now im building the pointer to the text
 bl TEXT_LOC
 mflr r4
 branchl REG_SCRATCH, Text_InitializeSubtext
